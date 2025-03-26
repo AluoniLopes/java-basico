83ed24b4eccb2f147f35bc1825c1f41401448bb5
Aluoní <105504582+AluoniLopes@users.noreply.github.com>
2025-03-26 14:35:46 -0300

    format: correcao de formatacao

--- src/edu/aluoni/basicjava/metodos/User.java
+++ src/edu/aluoni/basicjava/metodos/User.java
@@ -2,23 +2,22 @@ package edu.aluoni.basicjava.metodos;
 
 public class User {
     public static void main(String[] args) {
-        SmartTv smartTv = new SmartTv(12,13);
-    
+        SmartTv smartTv = new SmartTv(12, 13);
+
         System.out.println("Is on? " + smartTv.ligada);
         System.out.println("volume: " + smartTv.volume);
         System.out.println("channel: " + smartTv.canal);
         rule();
-        
+
         smartTv.ligar();
         System.out.println("Is on? " + smartTv.ligada);
         rule();
-        
+
         smartTv.desligar();
         System.out.println("Is on? " + smartTv.ligada);
     }
 
     private static void rule() {
         System.out.println("-----------------");
-    }    
+    }
 }
-    
